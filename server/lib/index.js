19d763d69af6e767b5c7ce926eb59a00bef7e268
Florian Margaine <florian@margaine.com>
2012-09-09 06:00:26 -0400

    Code supported-to-be-ready

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,53 +1,50 @@
 'use strict';
 
-var http = require( 'http' ),
-    server = http.createServer(),
-    Socket = require( './socket.js' );
+var io = require( 'socket.io' ).listen( 8080 ),
+    channels = {};
 
-    // The list of urls is quite thin
-    urls = [
-        '^/$',
-        '^/prez/server/*',
-        '^/prez/client/*',
-        '^/favicon.ico$'
-    ].map( function( url ) {
-        // Only create the new regex once
-        return new RegExp( url );
-    });
-
+/**
+ * This is how it works:
+ *   1. The extension connects to the websocket with a UUID.
+ *   2. This UUID is used to create a channel.
+ *   3. The websocket waits for the other client to connect with the same UUID.
+ *   4. Once it's done, it listens to the second client's commands and sends
+ *   them to the first client.
+ *
+ * The `channels` object contains all the clients in such a structure:
+ *   {
+ *       'UUID': [ client1, client2 ],
+ *       'UUID2': [ client3, client4 ]
+ *   }
+ */
 function init() {
-    // Initialize the static server
-    server.on( 'request', function( req, res ) {
-        // Check if we have the URL
-        var success = urls.some( function( url ) {
-            if ( url.exec( req.url ) ) {
-                return true;
-            }
-            else {
-                return false;
-            }
+    io.sockets.on( 'connection', function( socket ) {
+        // Listen to the "create channel" even sent from the extension
+        socket.on( 'create channel', function( channel ) {
+            channels[ channel ].push( socket.id );
         });
 
-        // If we do, success!
-        if ( success ) {
-            var staticServer = require( './static.js' ),
-            staticServer.serve( req.url, req, res );
-        }
-        else {
-            console.log( 'Miss ' + req.url );
-            res.writeHead( 404 );
-            res.end();
-        }
-    });
+        // Listen to the second client connection
+        socket.on( 'join channel', function( channel ) {
+            channels[ channel ].push( socket.id );
+        });
 
-    // Initialize the socket
-    var socket = new Socket();
+        // Listen to the "send key" event from the second client
+        // and sent it to the first client
+        socket.on( 'send key', function( obj ) {
+            // Get the channel to send it on
+            var channel = channels[ obj.channel ];
+
+            // Get the first client of this channel
+            var client = channel[ 0 ];
+
+            // And send the key to it
+            io.sockets.socket( client ).emit( obj.key );
+        });
+    });
 }
 
 module.exports = {
-    listen: function( port ) {
-        init();
-        server.listen( port );
-    }
+    init: init
 };
 
