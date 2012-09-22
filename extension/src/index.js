107f16ad34e037dcd7249fb186731a8a475910ac
Florian Margaine <florian@margaine.com>
2012-09-22 12:14:32 +0200

    Added an error in case no engine is found

--- extension/src/index.js
+++ extension/src/index.js
@@ -10,7 +10,7 @@ var uuid = require( 'node-uuid' ),
  *   - Create a channel which name is a generated UUID.
  *   - Listen on "send key" event from the websocket to get
  *   a keyCode.
- *   - Dispatch a KeyboardEvent using this keyCode.
+ *   - Inject some code to run the next/previous command
  */
 
 var socket = io.connect( 'http://remoteprez.margaine.com:8080/' );
@@ -18,45 +18,50 @@ var socket = io.connect( 'http://remoteprez.margaine.com:8080/' );
 var channel = uuid.v4(),
     engine = engines.guess();
 
-socket.on( 'connect', function() {
-    // Emit an event to create the channel
-    socket.emit( 'create channel', channel );
+if ( !engine ) {
+    alert( "You're not on a supported presentation page. Sorry." );
+}
+else {
+    socket.on( 'connect', function() {
+        // Emit an event to create the channel
+        socket.emit( 'create channel', channel );
 
-    // Display the link/qrcode
-    require( './html.js' )( channel, engine );
+        // Display the link/qrcode
+        require( './html.js' )( channel, engine );
 
-    // Listen on the "send key" event
-    socket.on( 'send direction', function( engine, direction ) {
-        // Mapping object
-        var mapping = {
-            'impress.js': {
-                'top': 'impress().prev()',
-                'bottom': 'impress().next()',
-                'left': 'impress().prev()',
-                'right': 'impress().next()'
-            },
-            'reveal.js': {
-                'top': 'Reveal.navigateUp()',
-                'bottom': 'Reveal.navigateDown()',
-                'left': 'Reveal.navigateLeft()',
-                'right': 'Reveal.navigateRight()'
-            },
-            'html5slides': {
-                'top': 'prevSlide()',
-                'bottom': 'nextSlide()',
-                'left': 'prevSlide()',
-                'right': 'nextSlide()'
-            },
-            'csss': {
-                'top': 'slideshow.previous()',
-                'bottom': 'slideshow.next()',
-                'left': 'slideshow.previous()',
-                'right': 'slideshow.next()'
-            }
-        };
+        // Listen on the "send key" event
+        socket.on( 'send direction', function( engine, direction ) {
+            // Mapping object
+            var mapping = {
+                'impress.js': {
+                    'top': 'impress().prev()',
+                    'bottom': 'impress().next()',
+                    'left': 'impress().prev()',
+                    'right': 'impress().next()'
+                },
+                'reveal.js': {
+                    'top': 'Reveal.navigateUp()',
+                    'bottom': 'Reveal.navigateDown()',
+                    'left': 'Reveal.navigateLeft()',
+                    'right': 'Reveal.navigateRight()'
+                },
+                'html5slides': {
+                    'top': 'prevSlide()',
+                    'bottom': 'nextSlide()',
+                    'left': 'prevSlide()',
+                    'right': 'nextSlide()'
+                },
+                'csss': {
+                    'top': 'slideshow.previous()',
+                    'bottom': 'slideshow.next()',
+                    'left': 'slideshow.previous()',
+                    'right': 'slideshow.next()'
+                }
+            };
 
-        // Just inject the right function
-        util.injectCode( mapping[ engine ][ direction ] );
+            // Just inject the right function
+            util.injectCode( mapping[ engine ][ direction ] );
+        });
     });
-});
+}
 
