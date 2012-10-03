7a910e0f4aa0c17c19fb8a93032650e8a54d5f15
Abhishek Hingnikar <djdark88.7@gmail.com>
2012-10-03 22:19:59 +0530

    Fixed style

--- public/assets/js/prez/index.js
+++ public/assets/js/prez/index.js
@@ -19,10 +19,11 @@ var channel = getParameterByName( 'c' ),
     engine = getParameterByName( 'e' );
 
 var socket = io.connect( 'http://remoteprez.margaine.com:8080/' );
+
 socket.once('reconnect',function(){
- socket.once('disconnect',function(){
-  io.transports = ["htmlfile","xhr-polling","jsonp-polling"];
- });
+    socket.once('disconnect',function(){
+        io.transports = ["htmlfile","xhr-polling","jsonp-polling"];
+    });
 });
 socket.once( 'connect', function() {
     // Emit an event to join the channel
